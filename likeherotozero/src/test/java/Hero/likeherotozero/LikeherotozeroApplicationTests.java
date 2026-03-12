d72fd4cd36a0dcaf70b782a9bf1b9843caed4000
Janakiin <59832231+Janakiin@users.noreply.github.com>
2026-03-12 13:47:45 +0100

    Create the project

---
+++ likeherotozero/src/test/java/Hero/likeherotozero/LikeherotozeroApplicationTests.java
@@ -0,0 +1,13 @@
+package Hero.likeherotozero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeherotozeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
